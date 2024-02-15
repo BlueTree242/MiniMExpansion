0a39648504a32529716553d3eae44ad9df9258cd
BlueTree242 <yahiamsm2@gmail.com>
2024-02-15 21:54:06 +0200

    Add relational placeholder support.

--- src/main/java/dev/bluetree242/minim/MiniMExpansion.java
+++ src/main/java/dev/bluetree242/minim/MiniMExpansion.java
@@ -2,6 +2,7 @@ package dev.bluetree242.minim;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.clip.placeholderapi.expansion.Relational;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
@@ -11,10 +12,10 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import java.util.Locale;
 
-public class MiniMExpansion extends PlaceholderExpansion {
+public class MiniMExpansion extends PlaceholderExpansion implements Relational {
     @Override
     public @NotNull String getIdentifier() {
-        return "PAPIMiniColors".toLowerCase(Locale.ROOT);
+        return "MiniM".toLowerCase(Locale.ROOT);
     }
 
     @Override
@@ -34,12 +35,22 @@ public class MiniMExpansion extends PlaceholderExpansion {
 
     @Override
     public @Nullable String onRequest(OfflinePlayer player, @NotNull String params) {
+        return convert(player, null, params);
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player one, Player two, String identifier) {
+        return convert(one, two, identifier);
+    }
+
+
+    private String convert(OfflinePlayer one, Player two, String params) {
         LegacyComponentSerializer serializer = LegacyComponentSerializer.legacyAmpersand();
         if (params.toLowerCase(Locale.ROOT).startsWith("section_")) {
             serializer = LegacyComponentSerializer.legacySection();
             params = params.substring(8);
         }
-        String result = PlaceholderAPI.setPlaceholders(player, "%" + params + "%");
+        String result = two != null && one instanceof Player ? PlaceholderAPI.setRelationalPlaceholders((Player) one, two, "%" + params + "%") : PlaceholderAPI.setPlaceholders(one, "%" + params + "%");
         if (result.equals("&r") || result.equals("§r")) return "<reset>";
         Component component = serializer.deserialize(result);
         return MiniMessage.miniMessage().serialize(component);
--- src/test/java/dev/bluetree242/minim/MiniMExpansionTest.java
+++ src/test/java/dev/bluetree242/minim/MiniMExpansionTest.java
@@ -17,10 +17,12 @@ import static org.mockito.Mockito.mockStatic;
 @ExtendWith(MockitoExtension.class)
 public class MiniMExpansionTest {
     private final Player player;
+    private final Player player2;
     private final MiniMExpansion expansion = new MiniMExpansion();
 
-    private MiniMExpansionTest(@Mock Player player) {
+    private MiniMExpansionTest(@Mock Player player, @Mock Player player2) {
         this.player = player;
+        this.player2 = player2;
     }
 
     @Test
@@ -47,9 +49,12 @@ public class MiniMExpansionTest {
         try (MockedStatic<PlaceholderAPI> mockedStatic = mockStatic(PlaceholderAPI.class)) {
             String placeholder = "%vault_prefix%";
             String placeholderExact = "vault_prefix";
-            mockedStatic.when(() -> PlaceholderAPI.setPlaceholders((OfflinePlayer) any(), eq(placeholder))).thenReturn(request);
+            mockedStatic.when(() -> PlaceholderAPI.setPlaceholders((OfflinePlayer) eq(player), eq(placeholder))).thenReturn(request);
             String result = expansion.onPlaceholderRequest(player, (section ? "section_" : "")  + placeholderExact);
+            mockedStatic.when(() -> PlaceholderAPI.setRelationalPlaceholders(eq(player), eq(player2), eq(placeholder))).thenReturn(request);
+            String resultRelational = expansion.onPlaceholderRequest(player, player2, (section ? "section_" : "")  + placeholderExact);
             assertEquals(result, expected);
+            assertEquals(resultRelational, expected);
         }
     }
 }
